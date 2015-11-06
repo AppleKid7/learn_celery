cf570b633069f3c65d4ec7e000495cbb965b8e55
Nick Thompson <nthompson@slb.com>
2015-11-06 13:22:31 -0600

    Concurrency explanation

--- basic_celery/tasks.py
+++ basic_celery/tasks.py
@@ -26,7 +26,7 @@ def fft_random(n):
     """
     This is just a huge number crunching task so that progress reporting won't be meaningless:
     """
-    fft_random.backend.mark_as_started(fft_random.request.id, progress=0)
+    #fft_random.backend.mark_as_started(fft_random.request.id, progress=0)
     for i in range(n):
         x = random.normal(0, 0.1, 2000)
         y = fft(x)
