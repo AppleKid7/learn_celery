2e249e77d242fee82c650244bc84089a3c768866
Nick Thompson <nthompson@slb.com>
2015-11-07 16:08:57 -0600

    Storing tasks in request.session

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This tutorial attempts to explain Celery + RabbitMQ to a beginner, with working 
 
 To compile the talk.tex file on Ubuntu, use:
 
-$ sudo apt-get install texlive-full
-$ sudo apt-get install python-pygments
-$ pdflatex -shell-escape talk.tex && evince talk.pdf
+.. code-block:: console
+
+    $ sudo apt-get install texlive-full
+    $ sudo apt-get install python-pygments
+    $ pdflatex -shell-escape talk.tex && evince talk.pdf
--- django_example/jobs/views.py
+++ django_example/jobs/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
-
 from django.views.generic import View
+
+from celery.result import AsyncResult
+
 from .tasks import fft_random
 
 class Home(View):
     def get(self, request):
-        return render(request, 'home.html', context={})
+        if 'task_id' in request.session.keys():
+            # That AsyncResult takes a constructor on
+            # the task_id is super-convenient:
+            t = AsyncResult(request.session['task_id'])
+            request.session['task_state'] = t.state
+
+        return render(request, 'home.html')
 
     def post(self, request):
         if 'ffts' in request.POST.keys():
             count = int(request.POST['ffts'])
-            # Without the .delay, no task is performed.
-            # fft_random(count)
-            # With .delay, a task is sent to celery:
-            fft_random.delay(count)
+            t = fft_random.delay(count)
+            request.session['task_id'] = t.task_id
+            request.session['task_state'] = t.state
 
-        print(request.POST)
-        return render(request, 'home.html', context={})
+        return render(request, 'home.html')
 
